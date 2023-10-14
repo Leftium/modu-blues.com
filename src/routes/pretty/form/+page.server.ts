9c0be078c5b4cbfdf6505f9cb698ae37e1932ce1
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-10-15 04:24:31 +0900

    Start rendering form.

--- src/routes/pretty/form/+page.server.ts
+++ src/routes/pretty/form/+page.server.ts
@@ -6,5 +6,24 @@ export const load = async ({ url, fetch }) => {
 	const resp = await fetch(fetchUrl);
 	const json = await resp.json();
 
-	return json;
+	// Adjust Google forms JSON:
+	const formJson = {
+		headerImageUrl: json.json.headerImageUrl,
+		collectEmails: json.json.collectEmails,
+		fields: json.json.questions
+	};
+
+	// Insert after first image/video:
+	formJson.fields.splice(1, 0, {
+		itemId: null,
+		title: json.json.title,
+		titleHtml: json.json.titleHtml,
+		description: json.json.description,
+		descriptionHtml: json.json.descriptionHtml,
+		type: 'TITLE_AND_DESCRIPTION',
+		options: [],
+		required: false
+	});
+
+	return { formJson };
 };
--- src/routes/pretty/form/+page.svelte
+++ src/routes/pretty/form/+page.svelte
@@ -5,5 +5,45 @@
 </script>
 
 <main class="container">
-	<pre>{JSON.stringify(data.json, null, 4)}</pre>
+	{#each data.formJson.fields as field}
+		<div>
+			{#if field.type === 'TITLE_AND_DESCRIPTION'}
+				<p>{@html field.titleHtml}</p>
+				{@html field.descriptionHtml}
+			{:else if field.type === 'IMAGE'}
+				<img src={field.imgUrl} />
+			{:else if field.type === 'VIDEO'}
+				<div class="wrap-youtube">
+					<iframe
+						class="youtube"
+						src="https://www.youtube.com/embed/{field.youtubeId}/?rel=0&controls=1&modestbranding=1"
+						allowfullscreen
+					/>
+				</div>
+			{:else}
+				TODO: {field.type}
+			{/if}
+		</div>
+	{/each}
+
+	<div hidden>
+		<hr />
+		<pre>{JSON.stringify(data.formJson, null, 4)}</pre>
+	</div>
 </main>
+
+<style>
+	:global(.wrap-youtube) {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding-bottom: 56.25%;
+	}
+	:global(.youtube) {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
