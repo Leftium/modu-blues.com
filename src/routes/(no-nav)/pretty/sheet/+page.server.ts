92664715a139be6e035e684b24af0007f583e1a6
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-11-25 00:27:45 +0900

    Add /pretty/sheet route.

---
+++ src/routes/(no-nav)/pretty/sheet/+page.server.ts
@@ -0,0 +1,57 @@
+export const load = async ({ url, fetch }) => {
+	const urlTarget = url.searchParams.get('u');
+
+	const fetchUrl = `/api/isr/${urlTarget}`;
+
+	const resp = await fetch(fetchUrl);
+	const json = await resp.json();
+
+	// Unwrap from inside meta data.
+	const sheetJson = json.json;
+
+	const columnNames = sheetJson.values.shift();
+
+	const counts = {
+		total: sheetJson.values.length,
+		leads: 0,
+		follows: 0
+	};
+
+	const rows: { summary: string; cells: string[] }[] = [];
+	sheetJson.values.forEach((cells: string[], rowIndex: number) => {
+		let summary = '';
+		let cheer = '';
+		let role = '';
+		let name = '';
+		let paid = '';
+
+		cells.forEach((cell, index) => {
+			const columnName = columnNames[index];
+
+			if (/^역할/.test(columnName)) {
+				if (cell === '리더') {
+					role = '🕺';
+					counts.leads++;
+				} else {
+					counts.follows++;
+					role = '💃';
+				}
+			} else if (/^입금확인/.test(columnName)) {
+				paid = cell ? '💰' : '';
+			} else if (/^응원/.test(columnName)) {
+				cheer = cell || '';
+			} else if (/^닉네임/.test(columnName)) {
+				name = cell || '';
+			}
+		});
+
+		summary = `<div class="cheer">${cheer}</div>${rowIndex + 1}. ${role}<b>${name}</b>`;
+
+		rows.push({
+			summary,
+			cells
+		});
+	});
+
+	return { counts, columnNames, rows, sheetJson };
+};
---
+++ src/routes/(no-nav)/pretty/sheet/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<main class="container">
+	<h1>
+		<center>{data.counts.total}명 신청 🕺{data.counts.leads} 💃{data.counts.follows}</center>
+	</h1>
+
+	{#each data.rows as row}
+		<hr />
+		<details>
+			<summary>
+				{@html row.summary}
+			</summary>
+			{#each row.cells as cell, index}
+				<b>{data.columnNames[index]}</b>
+				<p>{cell}</p>
+			{/each}
+		</details>
+	{/each}
+
+	<div hidden>
+		<pre>{JSON.stringify(data.counts, null, 4)}</pre>
+		<pre>{JSON.stringify(data.columnNames, null, 4)}</pre>
+		<pre>{JSON.stringify(data.rows, null, 4)}</pre>
+		<pre>{JSON.stringify(data.sheetJson.values, null, 4)}</pre>
+	</div>
+</main>
+
+<style>
+	main {
+		padding: 0.2rem 0;
+	}
+	summary {
+		line-height: 1.6rem;
+	}
+
+	summary :global(.cheer) {
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+		font-size: medium;
+		opacity: 66%;
+	}
+
+	details,
+	hr {
+		margin: 0.2rem 0;
+	}
+</style>
