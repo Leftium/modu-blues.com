61e68efe71b78c7db8e450676b5bc84da79b6abc
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2023-11-25 01:02:14 +0900

    Add u param redirect in handle().

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,18 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const u = event.url.searchParams.get('u');
+
+	if (u && event.url.pathname === '/') {
+		const uType = /^https:\/\/(forms.gle\/)|(docs.google.com\/forms\/d\/e)/.test(u)
+			? 'form'
+			: 'sheet';
+
+		const newUrl = new URL(`${event.url.origin}/pretty/${uType}`);
+		newUrl.searchParams.set('u', u);
+		throw redirect(302, newUrl);
+	}
+
 	const response = await resolve(event);
 	return response;
 };
