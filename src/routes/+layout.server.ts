356668205ed0673b3a9f42505232989a11cf1ec2
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-15 13:24:56 +0900

    Replace router google sheets ISR with json file.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,38 +1,13 @@
-import { error } from '@sveltejs/kit';
+import routes from './routes.json';
 
-export const load = async ({ fetch, url }) => {
+const routeMap: Record<string, unknown> = {};
+
+for (const { path, title, formUrl } of routes) {
+	routeMap[path] = { title, formUrl };
+}
+
+export const load = async () => {
 	console.log('+layout.server load');
-	const resp = await fetch(
-		url.origin +
-			'/api/isr/https://docs.google.com/spreadsheets/d/1uFQ8W20dkHr01KB_zaoUkLU_1YhIAt7DtNOUNiHqKAk/edit'
-	);
 
-	if (resp.status === 200) {
-		// console.log(resp);
-		console.log('Getting JSON...');
-		const json = await resp.json();
-		// console.log({ json });
-
-		const values = [...json.json.values];
-		values.shift();
-
-		const routeMap: Record<string, unknown> = {};
-		const routes = values.map(([path, title, formUrl]) => {
-			path = '/' + path;
-
-			routeMap[path] = { title, formUrl };
-
-			return { path, title, formUrl };
-		});
-
-		return { routes, routeMap };
-	} else {
-		if (resp.status === 404) {
-			// console.log(resp);
-			const text = await resp.text();
-			/// console.log(text);
-		}
-
-		throw error(555);
-	}
+	return { routes, routeMap };
 };
